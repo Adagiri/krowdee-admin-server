9763f9e95577391973d094b87f2e99caeef0f344
Adagiri <ibrahimridwan47@gmail.com>
2021-06-05 18:18:19 +0100

    aws json file kill

--- aws/index.js
+++ aws/index.js
@@ -1,17 +1,13 @@
 import AWS from "aws-sdk";
 import { makeid } from "../helpers/functions/index.js";
-import awsKeys from "./awsKeys.js";
-
-const { accessKeyId, secretAccessKey } = awsKeys;
 
 const S3 = new AWS.S3({
-  accessKeyId,
-  secretAccessKey,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
 
 export const getSignedUrl = async (userId, contentType) => {
   try {
-    
     const key = `${userId}/${makeid(10)}.${contentType.slice(6)}`;
     const url = await S3.getSignedUrl("putObject", {
       Bucket: "krowdee-prime-123",
